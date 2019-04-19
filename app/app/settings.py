fdba73b23760487163213f24e299c559804a7578
Yeonwoo Park <ywoo1234@gmail.com>
2019-04-19 17:34:55 -0400

    Update settings and urls for GraphQL and Graphene

--- app/app/settings.py
+++ app/app/settings.py
@@ -39,7 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'tracks',
+    'graphene_django',
 ]
+
+GRAPHENE = {
+    'SCHEMA': 'app.schema.schema'
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
