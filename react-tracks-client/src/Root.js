a89c4c4288f1c46fa000af4b2a618c07952dab85
Yeonwoo Park <ywoo1234@gmail.com>
2019-04-22 19:45:53 -0400

    Add React client with Apollo client settings, including basic implementation with gql

---
+++ react-tracks-client/src/Root.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import withRoot from './withRoot';
+
+import { Query } from 'react-apollo';
+import { gql } from 'apollo-boost';
+
+const Root = () => (
+  <Query query={GET_TRACKS_QUERY}>
+    {({ data, loading, error }) => {
+      if (loading) return <p>Loading...</p>;
+      if (error) return <p>Error :(</p>;
+
+      return <div>{JSON.stringify(data)}</div>;
+    }}
+  </Query>
+);
+
+const GET_TRACKS_QUERY = gql`
+  {
+    tracks {
+      id
+      title
+      description
+      url
+    }
+  }
+`;
+
+export default withRoot(Root);
---
+++ react-tracks-client/src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Root from './Root';
+import * as serviceWorker from './serviceWorker';
+
+import { ApolloProvider } from 'react-apollo';
+import ApolloClient from 'apollo-boost';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:8000/graphql/'
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <Root />
+  </ApolloProvider>,
+  document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
