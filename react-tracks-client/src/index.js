68656d191dbf4dc4e5ab77c2bbcb9060c8e896a4
Yeonwoo Park <ywoo1234@gmail.com>
2019-04-22 20:04:33 -0400

    Add Register form. Render Auth component by default

--- react-tracks-client/src/index.js
+++ react-tracks-client/src/index.js
@@ -6,13 +6,15 @@ import * as serviceWorker from './serviceWorker';
 import { ApolloProvider } from 'react-apollo';
 import ApolloClient from 'apollo-boost';
 
+import Auth from './components/Auth';
+
 const client = new ApolloClient({
   uri: 'http://localhost:8000/graphql/'
 });
 
 ReactDOM.render(
   <ApolloProvider client={client}>
-    <Root />
+    <Auth />
   </ApolloProvider>,
   document.getElementById('root')
 );
