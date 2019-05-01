6ee35894375d07c9efde9361bbd672a9036aced7
Yeonwoo Park <ywoo1234@gmail.com>
2019-04-30 22:58:11 -0400

    Add Search functionality using ApolloConsumer with Search Track Query

--- react-tracks-client/src/components/Track/SearchTracks.js
+++ react-tracks-client/src/components/Track/SearchTracks.js
@@ -1,21 +1,76 @@
-import React from "react";
-import withStyles from "@material-ui/core/styles/withStyles";
-// import TextField from "@material-ui/core/TextField";
-// import ClearIcon from "@material-ui/icons/Clear";
-// import Paper from "@material-ui/core/Paper";
-// import IconButton from "@material-ui/core/IconButton";
-// import SearchIcon from "@material-ui/icons/Search";
+import React, { useState } from 'react';
+
+import { ApolloConsumer } from 'react-apollo';
+import { gql } from 'apollo-boost';
+
+import withStyles from '@material-ui/core/styles/withStyles';
+import TextField from '@material-ui/core/TextField';
+import ClearIcon from '@material-ui/icons/Clear';
+import Paper from '@material-ui/core/Paper';
+import IconButton from '@material-ui/core/IconButton';
+import SearchIcon from '@material-ui/icons/Search';
 
 const SearchTracks = ({ classes }) => {
-  return <div>SearchTracks</div>;
+  const [search, setSearch] = useState('');
+
+  const handleSubmit = async (event, client) => {
+    event.preventDefault();
+    const res = await client.query({
+      query: SEARCH_TRACKS_QUERY,
+      variables: { search }
+    });
+    console.log({ res });
+  };
+  return (
+    <ApolloConsumer>
+      {client => (
+        <form onSubmit={event => handleSubmit(event, client)}>
+          <Paper className={classes.root} elevation={1}>
+            <IconButton>
+              <ClearIcon />
+            </IconButton>
+            <TextField
+              fullWidth
+              placeholder="Search All Tracks"
+              InputProps={{
+                disableUnderline: true
+              }}
+              onChange={event => setSearch(event.target.value)}
+            />
+            <IconButton type="submit">
+              <SearchIcon />
+            </IconButton>
+          </Paper>
+        </form>
+      )}
+    </ApolloConsumer>
+  );
 };
+
+const SEARCH_TRACKS_QUERY = gql`
+  query($search: String) {
+    tracks(search: $search) {
+      id
+      title
+      description
+      url
+      likes {
+        id
+      }
+      postedBy {
+        id
+        username
+      }
+    }
+  }
+`;
 
 const styles = theme => ({
   root: {
-    padding: "2px 4px",
+    padding: '2px 4px',
     margin: theme.spacing.unit,
-    display: "flex",
-    alignItems: "center"
+    display: 'flex',
+    alignItems: 'center'
   }
 });
 
