1dca62b440c17318d81ee648035524db3c5db2bb
Yeonwoo Park <ywoo1234@gmail.com>
2019-04-30 20:25:58 -0400

    Add TrackList component and put it under App.js

--- react-tracks-client/src/pages/App.js
+++ react-tracks-client/src/pages/App.js
@@ -1,13 +1,53 @@
-import React from "react";
-import withStyles from "@material-ui/core/styles/withStyles";
+import React from 'react';
+import withStyles from '@material-ui/core/styles/withStyles';
+
+import { Query } from 'react-apollo';
+import { gql } from 'apollo-boost';
+
+import SearchTracks from '../components/Track/SearchTracks';
+import TrackList from '../components/Track/TrackList';
+import CreateTrack from '../components/Track/CreateTrack';
+import Loading from '../components/Shared/Loading';
+import Error from '../components/Shared/Error';
 
 const App = ({ classes }) => {
-  return <div>App</div>;
+  return (
+    <div className={classes.container}>
+      <SearchTracks />
+      <CreateTrack />
+      <Query query={GET_TRACKS_QUERY}>
+        {({ data, loading, error }) => {
+          if (loading) return <Loading />;
+          if (error) return <Error error={error} />;
+
+          return <TrackList tracks={data.tracks} />;
+        }}
+      </Query>
+    </div>
+  );
 };
+
+const GET_TRACKS_QUERY = gql`
+  query getTracksQuery {
+    tracks {
+      id
+      title
+      description
+      url
+      likes {
+        id
+      }
+      postedBy {
+        id
+        username
+      }
+    }
+  }
+`;
 
 const styles = theme => ({
   container: {
-    margin: "0 auto",
+    margin: '0 auto',
     maxWidth: 960,
     padding: theme.spacing.unit * 2
   }
